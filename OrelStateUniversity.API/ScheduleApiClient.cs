8a4714699c7a349d2ef747df28e648de2f3feb30
XDenizX <denizik1@mail.ru>
2022-08-16 19:01:49 +0300

    Added a template for the schedule api client.

---
+++ OrelStateUniversity.API/ScheduleApiClient.cs
@@ -0,0 +1,31 @@
+﻿using OrelStateUniversity.API.Interfaces;
+using OrelStateUniversity.API.Models;
+
+namespace OrelStateUniversity.API;
+
+/// <summary>
+/// Represents a class with the implementation of interaction with Orel State University API
+/// with HttpClient.
+/// </summary>
+public class ScheduleApiClient : IScheduleApiClient
+{
+    public async Task<IEnumerable<Division>> GetDivisionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Course>> GetCoursesAsync(Division devision)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Group>> GetGroupsAsync(Division devision, Course course)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Schedule> GetScheduleAsync(Group group)
+    {
+        throw new NotImplementedException();
+    }
+}
