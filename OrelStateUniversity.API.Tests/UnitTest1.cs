4ade333f3857b07e406b21bbe36365cdae8db593
XDenizX <denizik1@mail.ru>
2022-08-16 19:12:50 +0300

    Added a nUnit test project.

---
+++ OrelStateUniversity.API.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace OrelStateUniversity.API.Tests
+{
+    public class Tests
+    {
+        private readonly ScheduleApiClient _client = new();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
