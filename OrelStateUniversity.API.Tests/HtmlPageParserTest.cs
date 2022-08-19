b815beef02ab9887f1aa1f9e929458af9e1e3202
XDenizX <denizik1@mail.ru>
2022-08-19 18:46:20 +0300

    Made HTML page helper as static.

--- OrelStateUniversity.API.Tests/HtmlPageParserTest.cs
+++ OrelStateUniversity.API.Tests/HtmlPageParserTest.cs
@@ -1,29 +1,28 @@
 ﻿using OrelStateUniversity.API.Helpers;
-using System.Security.Cryptography;
 
 namespace OrelStateUniversity.API.Tests;
 
 internal class HtmlPageParserTest
 {
-    private HtmlPageParser _htmlPageParser;
+    private string _content;
 
     [SetUp]
     public void Setup()
     {
-        _htmlPageParser = new HtmlPageParser("\t\tfunction toNumbers(d){var e=[];d.replace(/(..)" +
+        _content = "\t\tfunction toNumbers(d){var e=[];d.replace(/(..)" +
             "/g,function(d){e.push(parseInt(d,16))});return e}function toHex(){for(var d=[],d=1==" +
             "arguments.length&&arguments[0].constructor==Array?arguments[0]:arguments,e=\"\",f=0;" +
             "f<d.length;f++)e+=(16>d[f]?\"0\":\"\")+d[f].toString(16);return e.toLowerCase()}var " +
             "a=toNumbers(\"6d707d2540b4193976cbb5f2b8772adb\"),b=toNumbers(\"e7180e699b02bcafba97" +
             "e1c4c390e063\"),c=toNumbers(\"fad0b5fb452efac6040b3e64eba04d33\");document.cookie=\"" +
             "BPC=\"+toHex(slowAES.decrypt(c,2,a,b))+\"; expires=Thu, 31-Dec-37 23:55:55 GMT; path" +
-            "=/\";document.location.href=\"https://foobar.com\";");
+            "=/\";document.location.href=\"https://foobar.com\";";
     }
 
     [Test]
     public void GetVariableValueATest()
     {
-        string variableValue = _htmlPageParser.GetVariableValue("a");
+        string variableValue = HtmlPageHelper.GetVariableValue(_content, "a");
         string expectedValue = "6d707d2540b4193976cbb5f2b8772adb";
 
         Assert.That(variableValue, Is.EqualTo(expectedValue));
@@ -32,7 +31,7 @@ internal class HtmlPageParserTest
     [Test]
     public void GetVariableValueBTest()
     {
-        string variableValue = _htmlPageParser.GetVariableValue("b");
+        string variableValue = HtmlPageHelper.GetVariableValue(_content, "b");
         string expectedValue = "e7180e699b02bcafba97e1c4c390e063";
 
         Assert.That(variableValue, Is.EqualTo(expectedValue));
@@ -41,7 +40,7 @@ internal class HtmlPageParserTest
     [Test]
     public void GetVariableValueCTest()
     {
-        string variableValue = _htmlPageParser.GetVariableValue("c");
+        string variableValue = HtmlPageHelper.GetVariableValue(_content, "c");
         string expectedValue = "fad0b5fb452efac6040b3e64eba04d33";
 
         Assert.That(variableValue, Is.EqualTo(expectedValue));
@@ -53,9 +52,7 @@ internal class HtmlPageParserTest
         string htmlPageCode = "<html>\r\n<head>\r\n\t<meta charset=\"utf-8\" />\r\n</head>\r\n" +
             "<body>\r\n    <h1>Test</h1>\r\n</body>\r\n</html>";
 
-        var htmlPageParser = new HtmlPageParser(htmlPageCode);
-
-        Assert.That(htmlPageParser.IsHtmlPage(), Is.True);
+        Assert.That(HtmlPageHelper.IsHtmlPage(htmlPageCode), Is.True);
     }
 
     [Test]
@@ -63,8 +60,6 @@ internal class HtmlPageParserTest
     {
         string htmlPageCode = "<script></script>";
 
-        var htmlPageParser = new HtmlPageParser(htmlPageCode);
-
-        Assert.That(htmlPageParser.IsHtmlPage(), Is.False);
+        Assert.That(HtmlPageHelper.IsHtmlPage(htmlPageCode), Is.False);
     }
 }
---
+++ OrelStateUniversity.API/Helpers/HtmlPageHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OrelStateUniversity.API.Helpers;
+
+/// <summary>
+/// Represents the static helper class for interacting with the HTML code of the page.
+/// </summary>
+public static class HtmlPageHelper
+{
+    public static string GetVariableValue(string pageContent, string variableName)
+    {
+        var regex = new Regex($"{variableName}[\\s]*=[\\s]*toNumbers[\\s]*\\([\\s]*\"(?<value>\\w*)\"[\\s]*\\)");
+        
+        Match match = regex.Match(pageContent);
+        
+        return match.Groups["value"].Value;
+    }
+
+    public static bool IsHtmlPage(string pageContent)
+    {
+        return pageContent.Contains("<html>");
+    }
+}
