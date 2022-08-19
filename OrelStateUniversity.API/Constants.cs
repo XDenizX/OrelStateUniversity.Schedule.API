9a4a9a107126217f9a044c0a257fc334db9d986a
XDenizX <denizik1@mail.ru>
2022-08-19 18:47:20 +0300

    Added a class with constants.

---
+++ OrelStateUniversity.API/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace OrelStateUniversity.API;
+
+public class Constants
+{
+    public const string CookieName = "BPC";
+    public const string CookiePath = "/";
+    public const string CookieDomain = "oreluniver.ru";
+
+    public static readonly DateTime CookieExpirationDate = new DateTime(2037, 12, 31, 23, 55, 55);
+    
+    public const string StudentsScheduleEndpoint = "https://oreluniver.ru/schedule/divisionlistforstuds";
+}
