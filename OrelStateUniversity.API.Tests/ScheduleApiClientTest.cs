0a51329f9bd3307cc73aaa996e7718401d2f6375
XDenizX <denizik1@mail.ru>
2022-08-21 20:44:06 +0300

    Added a test for schedule API client.

---
+++ OrelStateUniversity.API.Tests/ScheduleApiClientTest.cs
@@ -0,0 +1,20 @@
+﻿namespace OrelStateUniversity.API.Tests;
+
+internal class ScheduleApiClientTest
+{
+    private ScheduleApiClient _scheduleApiClient;
+
+    [SetUp]
+    public void Setup()
+    {
+        _scheduleApiClient = new ScheduleApiClient();
+    }
+
+    [Test]
+    public void GetScheduleTest()
+    {
+        var divisions = _scheduleApiClient.GetStudentDivisionsAsync().Result;
+        var courses = _scheduleApiClient.GetCoursesAsync(divisions.First()).Result;
+        var groups = _scheduleApiClient.GetGroupsAsync(divisions.First(), courses.First()).Result;
+    }
+}
