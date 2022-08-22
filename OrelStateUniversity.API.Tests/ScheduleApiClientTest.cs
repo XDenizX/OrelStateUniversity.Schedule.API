776004f104fe7a31a1c9698dbeda3185cd15faf6
XDenizX <denizik1@mail.ru>
2022-08-22 18:28:02 +0300

    Added tests for the schedule API client.

--- OrelStateUniversity.API.Tests/ScheduleApiClientTest.cs
+++ OrelStateUniversity.API.Tests/ScheduleApiClientTest.cs
@@ -1,4 +1,6 @@
-﻿namespace OrelStateUniversity.API.Tests;
+﻿using OrelStateUniversity.API.Models;
+
+namespace OrelStateUniversity.API.Tests;
 
 internal class ScheduleApiClientTest
 {
@@ -16,5 +18,38 @@ internal class ScheduleApiClientTest
         var divisions = _scheduleApiClient.GetStudentDivisionsAsync().Result;
         var courses = _scheduleApiClient.GetCoursesAsync(divisions.First()).Result;
         var groups = _scheduleApiClient.GetGroupsAsync(divisions.First(), courses.First()).Result;
+        var schedule = _scheduleApiClient.GetScheduleAsync(groups.First()).Result;
+    }
+
+    [Test]
+    public void GetCoursesArgumentNullExceptionTest()
+    {
+        Assert.That(
+            () => _scheduleApiClient.GetCoursesAsync(null),
+            Throws.ArgumentNullException);
+
+        Assert.That(
+            () => _scheduleApiClient.GetCoursesAsync(new Division { Id = 1 }),
+            Throws.Nothing);
+    }
+
+    [Test]
+    public void GetGroupsArgumentNullExceptionTest()
+    {
+        Assert.That(
+            () => _scheduleApiClient.GetGroupsAsync(null, new Course { Number = 1 }),
+            Throws.ArgumentNullException);
+
+        Assert.That(
+            () => _scheduleApiClient.GetGroupsAsync(new Division { Id = 1 }, null),
+            Throws.ArgumentNullException);
+
+        Assert.That(
+            () => _scheduleApiClient.GetGroupsAsync(null, null),
+            Throws.ArgumentNullException);
+
+        Assert.That(
+            () => _scheduleApiClient.GetGroupsAsync(new Division { Id = 1 }, new Course { Number = 1 }),
+            Throws.Nothing);
     }
 }
