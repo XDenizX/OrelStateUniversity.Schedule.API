2ff31dbcd4187e3481041940b7cc7796efee4de3
XDenizX <denizik1@mail.ru>
2022-08-23 23:27:44 +0300

    Added a date time helper.

---
+++ OrelStateUniversity.API/Helpers/DateTimeHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace OrelStateUniversity.API.Helpers;
+
+public static class DateTimeHelper
+{
+    public static long ConvertToLong(DateTime dateTime)
+    {
+        return new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
+    }
+}
