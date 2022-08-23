3a246c4e2aef4e4edc3ba50af5223d8fa788d2a4
XDenizX <denizik1@mail.ru>
2022-08-23 23:28:04 +0300

    Added tests for date time helper.

---
+++ OrelStateUniversity.API.Tests/DateTimeHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using OrelStateUniversity.API.Helpers;
+
+namespace OrelStateUniversity.API.Tests;
+
+internal class DateTimeHelperTest
+{
+    [Test]
+    public void ConvertZeroToLongTest()
+    {
+        var ms = DateTimeHelper.ConvertToLong(DateTime.UnixEpoch);
+
+        Assert.That(ms, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ConvertOneDayToLongTest()
+    {
+        var ms = DateTimeHelper.ConvertToLong(DateTime.UnixEpoch + TimeSpan.FromDays(1));
+
+        Assert.That(ms, Is.EqualTo(86400000));
+    }
+}
