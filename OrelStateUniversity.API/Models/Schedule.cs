d4051627738103ed693022b92e6a780af0bc5822
XDenizX <denizik1@mail.ru>
2022-08-24 19:25:53 +0300

    Added a schedule class.

--- OrelStateUniversity.API/Models/Schedule.cs
+++ OrelStateUniversity.API/Models/Schedule.cs
@@ -1,8 +1,13 @@
-﻿namespace OrelStateUniversity.API.Models;
+﻿using Newtonsoft.Json;
+using OrelStateUniversity.API.Converters;
+
+namespace OrelStateUniversity.API.Models;
 
 /// <summary>
 /// Represents a class for storing schedule information.
 /// </summary>
+[JsonConverter(typeof(ScheduleJsonConverter))]
 public class Schedule
 {
+    public List<Lesson> Lessons { get; set; } = new();
 }
