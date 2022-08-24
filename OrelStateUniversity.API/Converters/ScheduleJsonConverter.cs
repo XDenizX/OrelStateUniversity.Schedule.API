366977f0798ea420b6dd4edf7e20da7c7278dacc
XDenizX <denizik1@mail.ru>
2022-08-24 19:25:11 +0300

    Implemented a custom JSON converter for schedule object.

---
+++ OrelStateUniversity.API/Converters/ScheduleJsonConverter.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OrelStateUniversity.API.Models;
+
+namespace OrelStateUniversity.API.Converters;
+
+internal class ScheduleJsonConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(Schedule);
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        var schedule = new Schedule();
+
+        // Return an empty schedule when an array is received.
+        if (reader.TokenType == JsonToken.StartArray)
+        {
+            reader.Skip();
+            return schedule;
+        }
+        
+        JToken root = JToken.Load(reader);
+        foreach (JToken token in root.Values())
+        {
+            if (token == null)
+            {
+                continue;
+            }
+
+            var lesson = JsonConvert.DeserializeObject<Lesson>(token.ToString());
+            schedule.Lessons.Add(lesson);
+        }    
+
+        return schedule;
+    }
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
